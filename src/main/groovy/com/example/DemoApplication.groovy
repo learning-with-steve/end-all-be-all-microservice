13b39fc09aa78d37e6d3dfe63b3f34c8c1c29a0f
Steve Good <steve.good@am.sony.com>
2016-12-15 11:33:35 -0800

    Added hello world stuff

--- src/main/groovy/com/example/DemoApplication.groovy
+++ src/main/groovy/com/example/DemoApplication.groovy
@@ -1,12 +1,20 @@
 package com.example
 
+import groovy.util.logging.Slf4j
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+@Slf4j
 @SpringBootApplication
-class DemoApplication {
+class DemoApplication implements CommandLineRunner {
 
 	static void main(String[] args) {
 		SpringApplication.run DemoApplication, args
+	}
+
+	@Override
+	void run(String... args) throws Exception {
+		log.info 'Hello, world!'
 	}
 }

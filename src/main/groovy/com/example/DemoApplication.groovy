db36243d2c568c18154209ef58d59b23fa52021f
Steve Good <steve.good@am.sony.com>
2016-12-15 11:55:52 -0800

    Made changes per Robert (who hates code)

--- src/main/groovy/com/example/DemoApplication.groovy
+++ src/main/groovy/com/example/DemoApplication.groovy
@@ -1,27 +1,12 @@
 package com.example
 
-import groovy.util.logging.Slf4j
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@Slf4j
 @SpringBootApplication
-class DemoApplication implements CommandLineRunner {
+class DemoApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run DemoApplication, args
-	}
-
-	@Override
-	void run(String... args) throws Exception {
-        log.info 'Go Speed Racer Go'
-		log.debug "The time is now ${new Date()}"
-        (100..0).each {
-            log.info it
-        }
-
-		assert 'Steve Good'.reverse() == 'dooG evetS'
-		print "Hello!".chars().sum()
 	}
 }
